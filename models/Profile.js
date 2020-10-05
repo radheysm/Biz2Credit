09f3ef64aeda9a2f668c3ca31374a0828efa95cb
radheysm <radheysaraan@gmail.com>
2020-10-05 23:17:06 +0530

    backend

---
+++ models/Profile.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Profile = mongoose.model('profile', profileSchema);
--- routes/api/v1/profile.js
+++ routes/api/v1/profile.js
@@ -1,14 +1,116 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const passport = require('passport');
 const { check, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../../../middleware/auth');
+// Load Profile Model
+const Profile = require('../../../models/Profile');
+// Load User Model
+const User = require('../../../models/User');
 
-// @route     POST api/v1/profile
-// @desc      Create User
-// @access    Public
+// @route   GET api/v1/profile;
+// @desc    Get users
+// @access  Public
+router.get('/', auth, async (req, res) => {
+  try {
+    const profiles = await Profile.find().sort({ date: -1 });
+    res.json(profiles);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
-router.get('/', (req, res) => res.send('Profile route'));
+// @route   Post api/v1/profile;
+// @desc    Get users
+// @access  Public
+
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('name', 'Name is required').not().isEmpty(),
+      check('email', 'Please enter a valid email address').isEmail(),
+      check('phone', 'Please enter a valid 10 digit phone number ')
+        .not()
+        .isEmpty()
+        .isLength({ min: 10, max: 10 }),
+      check(
+        'password',
+        'please enter a  password with 3 or more characker'
+      ).isLength({ min: 3, max: 20 }),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // console.log('profile');
+    // console.log('route');
+    // console.log(req.body);
+    const { name, email, phone, password } = req.body;
+    const profileFields = {};
+    const user = await User.findById(req.user.id).select('-password');
+    profileFields.user = user.id;
+    if (name) profileFields.name = name;
+    if (email) profileFields.email = email;
+    if (phone) profileFields.phone = phone;
+    if (password) profileFields.password = password;
+    try {
+      let profile = await Profile.findOne({ user: user.id });
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      }
+      // const user = await User.findById(req.user.id).select('-password');
+      // const { name, email, phone, password } = req.body;
+      // console.log(user.id);
+      // const newProfile = new Profile({
+      //   name: req.body.name,
+      //   email: req.body.email,
+      //   phone: req.body.phone,
+      //   password: req.body.password,
+      //   user: user.id,
+      // });
+      // newProfile.save().then((profile) => res.json(profile));
+      //Create
+      profile = new Profile(profileFields);
+      await profile.save();
+      return res.json(profile);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error');
+    }
+
+    newUser.save().then((user) => res.json(user));
+  }
+);
+
+// @route   DELETE api/profile/:id
+// @desc    Delete post
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findById(req.params.id);
+    // Check user
+    if (profile.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not Authorized' });
+    }
+    await profile.remove();
+    res.json({ msg: 'User Removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
